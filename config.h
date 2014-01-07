24eae9bb2e8522d65ba4310db1c41130b7f1e179
segmentedbit <bit@bringthemail.com>
2014-01-08 00:15:48 +0100

    Cleaned averageFilter, added copyWithPadding and a global variable file

---
+++ config.h
@@ -0,0 +1,17 @@
+/*
+ * config.h
+ *
+ *  Created on: Jan 7, 2014
+ *      Author: fenrir
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+namespace config {
+const bool DEBUG = true;
+}
+
+
+
+#endif /* CONFIG_H_ */
